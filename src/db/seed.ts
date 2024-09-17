0cce2f10caf660c4301cb64ad214940fb75f39e9
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2024-09-17 09:50:10 -0300

    refactor(seed): email

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -55,7 +55,7 @@ const [manager] = await db
   .insert(users)
   .values({
     name: faker.person.fullName(),
-    email: 'diego.schell.f@gmail.com',
+    email: 'thyagoribeiro608@gmail.com',
     role: 'manager',
   })
   .returning()
